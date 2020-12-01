631c05013ff528250a638535df9f4d220c012f1d
Imar Abreu Díaz <abreudiazimar9620@gmail.com>
2020-12-01 21:12:58 +0000

    ADD props

--- src/components/Storyline.tsx
+++ src/components/Storyline.tsx
@@ -1,6 +1,5 @@
-import { getRegisteredStyles } from "@emotion/css";
-import styled from "@emotion/styled";
-import { FC, useEffect, useState } from "react";
+import styled from '@emotion/styled';
+import { FC, useState } from 'react';
 
 const Container = styled.div`
   background: ${props => props.color};
@@ -22,28 +21,17 @@ const HR = styled.hr`
   border-color: grey;
 `
 
-export const Storyline: FC = () => {
-  const [storyline, setStoryline] = useState('');
-  const [keywords, setKeywords] = useState([""]);
-  const [genres, setGenres] = useState([""]);
-  const COLOR ="#f7f3f2"
+interface StorylineProps {
+  storyline: string,
+  keywords: string[],
+  genres: string[]
+}
 
-  useEffect(() => {
-    setStoryline(`84 years later, a 100 year-old woman named 
-    Rose DeWitt Bukater tells the story to her granddaughter 
-    Lizzy Calvert, Brock Lovett, Lewis Bodine, Bobby Buell 
-    and Anatoly Mikailavich on the Keldysh about her life 
-    set in April 10th 1912, on a ship called Titanic 
-    when young Rose boards the departing ship with 
-    the upper-class passengers and her mother, 
-    Ruth DeWitt Bukater, and her fiancé, Caledon Hockley. 
-    Meanwhile, a drifter and artist named Jack Dawson and 
-    his best friend Fabrizio De Rossi win third-class tickets 
-    to the ship in a game. And she explains the whole story 
-    from departure until the death of Titanic on its first and last voyage April 15th, 1912 at 2:20 in the morning.`)
-    setKeywords(['iceberg', 'death', 'mass death', 'titanic', 'wet'])
-    setGenres(["Drama", "Romance"])
-  }, [])
+export const Storyline: FC<StorylineProps> = (props) => {
+  const [storyline, setStoryline] = useState(props.storyline);
+  const [keywords, setKeywords] = useState(props.keywords);
+  const [genres, setGenres] = useState(props.genres);
+  const COLOR = '#f7f3f2';
 
   return <Container color={COLOR}>
     <h2>Storyline</h2>
